c87dd7bf92d1a049e999290d3e1bec4e4563eeb4
Pranto Kumar <prantokumar.net@gmail.com>
2025-02-07 21:36:25 +0600

    deleted

--- CyberSec/subprocessPip.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-result = subprocess.run(["ipconfig/all"], capture_output=True, text=True, shell=True)
-
-with open("ipconfig.txt", "w") as ipAdress:
-  ipAdress.write(result.stdout)
-
